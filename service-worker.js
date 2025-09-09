3eb04d84ed1e7b9fb5f6085fdf857a3fa20e967d
Azerty-1977 <85751420+Azerty-1977@users.noreply.github.com>
2025-09-09 15:34:51 +0100

    Add files via upload
    Ajout des fichiers initiaux

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'gestion-stock-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
